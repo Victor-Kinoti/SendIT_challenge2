07038682d567ca923c3528193d3fed0ed980a80f
Victor-Kinoti <vickeynote@gmail.com>
2018-11-22 07:26:25 +0300

    [Start #162147585] admin can view a single order

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
 from .views.UserViews import DataParcel, SingleParcel, UsersOrders, RegisterUser, UserLogin
-from .views.AdminViews import OrderParcels
+from .views.AdminViews import OrderParcels, SingleOrder
 
 version2 = Blueprint('v2', __name__, url_prefix='/api/v2')
 
@@ -13,3 +13,4 @@ api.add_resource(UsersOrders, '/parcels/<user_id>/orders')
 api.add_resource(RegisterUser, '/register')
 api.add_resource(UserLogin, '/login')
 api.add_resource(OrderParcels, '/orders')
+api.add_resource(SingleOrder, '/orders/<order_id>')
